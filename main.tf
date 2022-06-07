e958b29214053d7e919cce260b884c58821aa38f
Prem Kumar <pkailasu@sph.com.sg>
2022-06-07 17:13:49 +0800

    Allow access from control plane to webhook port of AWS load balancer controller

--- main.tf
+++ main.tf
@@ -49,6 +49,14 @@ module "eks" {
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
     }
+    ingress_allow_access_from_control_plane = {
+      type                          = "ingress"
+      protocol                      = "tcp"
+      from_port                     = 9443
+      to_port                       = 9443
+      source_cluster_security_group = true
+      description                   = "Allow access from control plane to webhook port of AWS load balancer controller"
+    }
   }, var.node_security_group_additional_rules)
 
   cluster_encryption_config = [{
