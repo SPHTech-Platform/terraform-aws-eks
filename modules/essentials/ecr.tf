8d3687f6a84ce9aa61670638e856ed9ad84f35ea
bountyhunter05 <46589568+santhoshratala@users.noreply.github.com>
2022-06-28 13:21:23 +0800

    Always create ECR Pull Through Cache Policy (#21)
    * update configuration for ecr cache
    
    * update readme
    
    * update ecr essentials
    
    * update readme
    
    * update var type
    
    * rollback changes
    
    Co-authored-by: sratala <sratala@BI-P3KF4QH7FL.users.sphinet.com.sg>
    Co-authored-by: Niro <46478191+niroz89@users.noreply.github.com>
    Co-authored-by: sratala <sratala@BI-P3KF4QH7FL.local>

--- modules/essentials/ecr.tf
+++ modules/essentials/ecr.tf
@@ -1,12 +1,12 @@
 resource "aws_ecr_pull_through_cache_rule" "cache" {
-  for_each = var.configure_ecr_pull_through && var.configure_ecr_pull_through_policy ? var.ecr_pull_through_cache_rules : {}
+  for_each = var.configure_ecr_pull_through ? var.ecr_pull_through_cache_rules : {}
 
   ecr_repository_prefix = each.value.prefix
   upstream_registry_url = each.value.registry
 }
 
 data "aws_iam_policy_document" "ecr_cache" {
-  count = var.configure_ecr_pull_through_policy ? 1 : 0
+  count = var.configure_ecr_pull_through ? 1 : 0
 
   statement {
     actions = [
@@ -21,7 +21,7 @@ data "aws_iam_policy_document" "ecr_cache" {
 }
 
 resource "aws_iam_policy" "ecr_cache" {
-  count = var.configure_ecr_pull_through_policy ? 1 : 0
+  count = var.configure_ecr_pull_through ? 1 : 0
 
   name = var.ecr_cache_iam_cache_policy
 
@@ -31,7 +31,7 @@ resource "aws_iam_policy" "ecr_cache" {
 
 
 resource "aws_iam_role_policy_attachment" "worker_ecr_pullthrough" {
-  count = var.configure_ecr_pull_through_policy ? 1 : 0
+  count = var.configure_ecr_pull_through ? 1 : 0
 
   role       = var.worker_iam_role_name
   policy_arn = aws_iam_policy.ecr_cache[0].arn
