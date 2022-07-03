9d37605234416f3ecb552536424b3f7977f725df
niroz89 <niro@sph.com.sg>
2022-07-04 02:37:48 +0800

    Bump version

--- modules/eks_managed_nodes/main.tf
+++ modules/eks_managed_nodes/main.tf
@@ -65,7 +65,7 @@ locals {
 ################################################################################
 module "eks_managed_node_group" {
   source  = "terraform-aws-modules/eks/aws//modules/self-managed-node-group"
-  version = "~> 18.21.0"
+  version = "~> 18.26.0"
 
   for_each = local.eks_managed_node_groups
 
