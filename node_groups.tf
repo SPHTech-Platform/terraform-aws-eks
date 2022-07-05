8e4a4756cafcab72e15b40a8314791b35b085232
gowripadmajas <gowris@sph.com.sg>
2022-07-05 17:00:14 +0800

    remove trailing space

--- node_groups.tf
+++ node_groups.tf
@@ -134,7 +134,7 @@ resource "aws_cloudwatch_event_target" "node_termination_handler_spot" {
 }
 
 ################################################
-# Resources below are to enable Windows Support for Cluster. 
+# Resources below are to enable Windows Support for Cluster.
 ################################################
 
 locals {
