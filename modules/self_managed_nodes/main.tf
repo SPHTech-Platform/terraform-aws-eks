007504173df6bf1dfdc7bfb9eca732c64344acc2
niroz89 <niro@sph.com.sg>
2022-07-07 09:29:39 +0800

    Bump version everywhere

--- modules/self_managed_nodes/main.tf
+++ modules/self_managed_nodes/main.tf
@@ -53,7 +53,7 @@ locals {
 
 module "self_managed_group" {
   source  = "terraform-aws-modules/eks/aws//modules/self-managed-node-group"
-  version = "~> 18.21.0"
+  version = "~> 18.26.0"
 
   for_each = local.self_managed_node_groups
 
