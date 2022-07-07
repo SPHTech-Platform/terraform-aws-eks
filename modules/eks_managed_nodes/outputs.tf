0d5b57fad9b918b16e99ca4bc61a832befc17326
niroz89 <niro@sph.com.sg>
2022-07-07 08:38:58 +0800

    Expose node_group_labels only

--- modules/eks_managed_nodes/outputs.tf
+++ modules/eks_managed_nodes/outputs.tf
@@ -13,10 +13,10 @@
 #   value       = module.eks_managed_node_group.node_group_status
 # }
 
-# output "node_group_labels" {
-#   description = "Map of labels applied to the node group"
-#   value       = module.eks_managed_node_group.node_group_labels
-# }
+output "node_group_labels" {
+  description = "Map of labels applied to the node group"
+  value       = module.eks_managed_node_group.node_group_labels
+}
 
 # output "node_group_taints" {
 #   description = "List of objects containing information about taints applied to the node group"
