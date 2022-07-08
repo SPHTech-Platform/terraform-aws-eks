00fd5cd34e95572e5bf83f36916f37ce2e38e3d3
niroz89 <niro@sph.com.sg>
2022-07-08 11:10:13 +0800

    Convert the output to map

--- modules/eks_managed_nodes/outputs.tf
+++ modules/eks_managed_nodes/outputs.tf
@@ -1,5 +1,5 @@
 output "node_group_resources" {
-  description = "List of objects containing information about underlying resources"
+  description = "Map of objects containing information about underlying resources"
   value = {
     for k, v in module.eks_managed_node_group : k => v.node_group_resources
   }
@@ -7,21 +7,29 @@ output "node_group_resources" {
 }
 
 output "node_group_autoscaling_group_names" {
-  description = "List of the autoscaling group names"
-  value       = values(module.eks_managed_node_group)[*].node_group_autoscaling_group_names
+  description = "Map of the autoscaling group names"
+  value = {
+    for k, v in module.eks_managed_node_group : k => v.node_group_autoscaling_group_names
+  }
 }
 
 output "node_group_status" {
-  description = "Status of the EKS Node Group"
-  value       = values(module.eks_managed_node_group)[*].node_group_status
+  description = "Map of EKS Node Group status"
+  value = {
+    for k, v in module.eks_managed_node_group : k => v.node_group_status
+  }
 }
 
 output "node_group_labels" {
-  description = "Map of labels applied to the node group"
-  value       = values(module.eks_managed_node_group)[*].node_group_labels
+  description = "Map of labels applied to each node group"
+  value = {
+    for k, v in module.eks_managed_node_group : k => v.node_group_labels
+  }
 }
 
 output "node_group_taints" {
-  description = "List of objects containing information about taints applied to the node group"
-  value       = values(module.eks_managed_node_group)[*].node_group_taints
+  description = "Map objects containing information about taints applied to each node group"
+  value = {
+    for k, v in module.eks_managed_node_group : k => v.node_group_taints
+  }
 }
