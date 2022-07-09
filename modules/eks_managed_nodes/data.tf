612ed62b5045fac8e9159c609fa8a870d4d2d86f
niroz89 <niro@sph.com.sg>
2022-07-10 02:08:21 +0800

    Missing newline separator

--- modules/eks_managed_nodes/data.tf
+++ modules/eks_managed_nodes/data.tf
@@ -14,5 +14,6 @@ data "aws_subnet" "subnets" {
 
 data "aws_autoscaling_group" "node_groups" {
   for_each = module.eks_managed_node_group
-  name     = each.value.node_group_resources.0.autoscaling_groups.0.name
+
+  name = each.value.node_group_resources.0.autoscaling_groups.0.name
 }
