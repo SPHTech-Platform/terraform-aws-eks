7b531ede5a6c3ae53ea1a925a397274cfaf00dfe
niroz89 <niro@sph.com.sg>
2023-01-25 14:21:15 +0800

    Add missing dependency

--- versions.tf
+++ versions.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.0"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.2"
+    }
     # tflint-ignore: terraform_unused_required_providers
     kubernetes = {
       source  = "hashicorp/kubernetes"
