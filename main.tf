fc205e46d9afe06d781ef8b3e4fe90173e99b9f5
niroz89 <niro@sph.com.sg>
2023-03-15 11:30:27 +0800

    newline tfsec ignore

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
-#tfsec:ignore:aws-eks-no-public-cluster-access-to-cidr tfsec:ignore:aws-eks-no-public-cluster-access tfsec:ignore:aws-ec2-no-public-egress-sgr
+#tfsec:ignore:aws-eks-no-public-cluster-access-to-cidr
+#tfsec:ignore:aws-eks-no-public-cluster-access
+#tfsec:ignore:aws-ec2-no-public-egress-sgr
 #tfsec:ignore:aws-eks-enable-control-plane-logging
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
