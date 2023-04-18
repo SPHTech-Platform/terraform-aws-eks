587e8c111bd56e7fa91f69f407a3bb7ab9a6f5ea
Poh Peng <thepoppingone@users.noreply.github.com>
2023-04-18 18:19:50 +0800

    Remove cloudwatch resources creations

--- modules/fargate_profile/fargate_logger.tf
+++ modules/fargate_profile/fargate_logger.tf
@@ -47,3 +47,38 @@ locals {
     var.addon_config
   )
 }
+
+########################
+### K8s resources ######
+########################
+
+resource "kubernetes_namespace_v1" "aws_observability" {
+
+  count = var.fargate_logging_enabled ? 1 : 0
+
+  metadata {
+    name = "aws-observability"
+
+    labels = {
+      aws-observability = "enabled"
+    }
+  }
+}
+
+# fluent-bit-cloudwatch value as the name of the CloudWatch log group that is automatically created as soon as your apps start logging
+resource "kubernetes_config_map_v1" "aws_logging" {
+
+  count = var.fargate_logging_enabled ? 1 : 0
+
+  metadata {
+    name      = "aws-logging"
+    namespace = kubernetes_namespace_v1.aws_observability[0].id
+  }
+
+  data = {
+    "parsers.conf" = local.config["parsers_conf"]
+    "filters.conf" = local.config["filters_conf"]
+    "output.conf"  = local.config["output_conf"]
+  }
+
+}
