47950253e0cf28bda888ab70804e4e79245d9f7f
Poh Peng <thepoppingone@users.noreply.github.com>
2023-04-21 18:25:18 +0800

    Add karpenter as a submodule

---
+++ modules/karpenter/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.7"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10"
+    }
+  }
+}
