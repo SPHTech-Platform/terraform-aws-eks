d148e2f4d2b865a2d0e82a8eceb527087244994b
Poh Peng Ric Wang <7760361+thepoppingone@users.noreply.github.com>
2023-06-12 18:16:00 +0800

    Add vars for fargate ns (#63)
    * Add vars for fargate ns
    
    * add default values
    
    * Update readme
    
    ---------
    
    Co-authored-by: Poh Peng <thepoppingone@users.noreply.github.com>

--- node_groups.tf
+++ node_groups.tf
@@ -138,9 +138,11 @@ module "fargate_profiles" {
 
   count = var.fargate_cluster ? 1 : 0
 
-  cluster_name             = split("/", data.aws_arn.cluster.resource)[1]
-  fargate_profiles         = local.fargate_profiles
-  fargate_profile_defaults = var.fargate_profile_defaults
+  cluster_name                    = split("/", data.aws_arn.cluster.resource)[1]
+  fargate_profiles                = local.fargate_profiles
+  fargate_profile_defaults        = var.fargate_profile_defaults
+  create_aws_observability_ns     = var.create_aws_observability_ns
+  create_fargate_logger_configmap = var.create_fargate_logger_configmap
 
   tags = var.tags
 }
