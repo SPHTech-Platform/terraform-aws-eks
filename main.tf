5f8986963c681f9d1c1fc5ddabd4eaf5179ae490
Poh Peng <thepoppingone@users.noreply.github.com>
2023-06-16 14:46:29 +0800

    Outputs already pack all profiles into a single map

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ locals {
   aws_auth_fargate_profile_pod_execution_role_arns = var.fargate_cluster ? distinct(
     compact(
       concat(
-        [for profile in module.fargate_profiles : values(profile.fargate_profile_pod_execution_role_arn)],
+        values(module.fargate_profiles.fargate_profile_pod_execution_role_arn),
         var.aws_auth_fargate_profile_pod_execution_role_arns,
       )
     )
