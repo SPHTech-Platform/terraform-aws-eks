b4043088a355cd6643b8151079f3178dda12801b
Poh Peng <thepoppingone@users.noreply.github.com>
2023-06-27 16:45:06 +0800

    Add reference doc

--- main.tf
+++ main.tf
@@ -73,6 +73,7 @@ module "eks" {
       service_account_role_arn = module.vpc_cni_irsa_role.iam_role_arn
       configuration_values = jsonencode({
         env = {
+          # Reference doc: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html#security-groups-pods-deployment
           ENABLE_POD_ENI = "true"
         }
       })
