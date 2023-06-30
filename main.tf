a707a4f36e1dd5451593622fff4e0cf5fc661c16
Poh Peng <thepoppingone@users.noreply.github.com>
2023-06-30 21:53:38 +0800

    Add initEnv instead of Env

--- main.tf
+++ main.tf
@@ -83,7 +83,9 @@ module "eks" {
       configuration_values = jsonencode({
         env = {
           # Reference doc: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html#security-groups-pods-deployment
-          ENABLE_POD_ENI          = "true"
+          ENABLE_POD_ENI = "true"
+        }
+        InitEnv = {
           DISABLE_TCP_EARLY_DEMUX = "true"
         }
       })
