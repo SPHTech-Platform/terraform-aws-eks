14c1cfa10e6b85e429c9c24cf9eecca0ab5cc212
niroz89 <niro@sph.com.sg>
2023-07-06 16:22:43 +0530

    Fix fargate security group policy

--- fargate_profile.tf
+++ fargate_profile.tf
@@ -55,7 +55,7 @@ module "fargate_profiles" {
 }
 
 resource "kubernetes_manifest" "fargate_node_security_group_policy" {
-  count = var.fargate_cluster && var.create_node_security_group ? 1 : 0
+  for_each = var.fargate_cluster && var.create_node_security_group ? local.fargate_namespaces : {}
   manifest = {
     apiVersion = "vpcresources.k8s.aws/v1beta1"
     kind       = "SecurityGroupPolicy"
