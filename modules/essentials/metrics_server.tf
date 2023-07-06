1e836bc6d09180b3bd5a3496c2342f780f905507
Niro <46478191+niroz89@users.noreply.github.com>
2023-07-06 13:31:12 +0530

    Bump versions (#85)

--- modules/essentials/metrics_server.tf
+++ modules/essentials/metrics_server.tf
@@ -9,7 +9,7 @@ module "helm_metrics_server" {
   count = var.metrics_server_enabled ? 1 : 0
 
   source  = "SPHTech-Platform/release/helm"
-  version = "~> 0.1.0"
+  version = "~> 0.1.4"
 
   helm_config = local.metric_server_helm_config
 }
