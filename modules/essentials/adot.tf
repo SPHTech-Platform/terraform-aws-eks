654f135ee55455406e2036de031496d04285719a
Poh Peng <thepoppingone@users.noreply.github.com>
2023-07-24 17:18:42 +0800

    remove resolve_conflicts_on_update

--- modules/essentials/adot.tf
+++ modules/essentials/adot.tf
@@ -1,7 +1,6 @@
 resource "aws_eks_addon" "adot_operator" {
-  cluster_name                = var.cluster_name
-  addon_name                  = "adot"
-  resolve_conflicts_on_update = "OVERWRITE"
+  cluster_name = var.cluster_name
+  addon_name   = "adot"
 
   depends_on = [
     helm_release.cert_manager,
