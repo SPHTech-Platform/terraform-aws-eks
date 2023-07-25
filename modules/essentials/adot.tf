053b00caf0474681b318d210276a143e410b2472
Poh Peng <thepoppingone@users.noreply.github.com>
2023-07-25 10:15:47 +0800

    Add ADOT addon version to use latest by default but allow override;

--- modules/essentials/adot.tf
+++ modules/essentials/adot.tf
@@ -1,6 +1,17 @@
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
+data "aws_eks_addon_version" "latest_adot" {
+  addon_name         = "adot"
+  kubernetes_version = data.aws_eks_cluster.cluster.platform_version
+  most_recent        = true
+}
+
 resource "aws_eks_addon" "adot_operator" {
-  cluster_name = var.cluster_name
-  addon_name   = "adot"
+  cluster_name  = var.cluster_name
+  addon_name    = "adot"
+  addon_version = try(var.adot_addon_version, data.aws_eks_addon_version.latest_adot.version)
 
   depends_on = [
     helm_release.cert_manager,
