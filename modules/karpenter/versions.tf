61c1a8e7f41ee8fbc1addad8df4384d76a4198c8
Poh Peng <thepoppingone@users.noreply.github.com>
2023-10-06 18:42:36 +0800

    convert to kubectl

--- modules/karpenter/versions.tf
+++ modules/karpenter/versions.tf
@@ -6,13 +6,17 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 2.7"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.10"
-    }
+    # kubernetes = {
+    #   source  = "hashicorp/kubernetes"
+    #   version = ">= 2.10"
+    # }
     aws = {
       source  = "hashicorp/aws"
       version = ">= 4.47"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.14.0"
     }
   }
 }
