420c4602ff138460cd4a13b7ee4ebe597c6b1595
shisheng10 <shisheng@sph.com.sg>
2024-01-31 00:11:48 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -474,7 +474,14 @@ variable "karpenter_nodeclasses" {
     karpenter_node_tags_map                = map(string)
     karpenter_ami_family                   = string
     karpenter_node_user_data               = string
-    karpenter_node_metadata_options        = map(string)
+    karpenter_node_metadata_options = map(object({
+      httpEndpoint            = optional(string)
+      httpProtocolIpv6        = optional(string)
+      httpPutResponseHopLimit = optional(number)
+      httpTokens              = optional(string)
+      instanceMetadataTags    = optional(string)
+      })
+    )
     karpenter_block_device_mapping = list(object({
       deviceName = string
       ebs = object({
