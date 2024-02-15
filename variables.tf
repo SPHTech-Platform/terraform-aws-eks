044b04a363b8148d321f2499e3a7230d71bacf62
Uchinda Padmaperuma <89894943+uchinda-sph@users.noreply.github.com>
2024-02-15 17:29:10 +0800

    clean up the code

--- variables.tf
+++ variables.tf
@@ -156,11 +156,6 @@ variable "subnet_ids" {
   type        = list(string)
 }
 
-variable "subnet_selector_name_tag" {
-  description = "Subnet selector Name tags for Karpenter nodes, supports wildcard."
-  type        = string
-}
-
 variable "cluster_service_ipv4_cidr" {
   description = "The CIDR block to assign Kubernetes service IP addresses from. If you don't specify a block, Kubernetes assigns addresses from either the 10.100.0.0/16 or 172.20.0.0/16 CIDR blocks"
   type        = string
