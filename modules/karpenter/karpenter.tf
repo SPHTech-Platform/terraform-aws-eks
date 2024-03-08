ce51168783e0af00e3c923e3491f8b1847e2f76c
Uchinda Padmaperuma <89894943+uchinda-sph@users.noreply.github.com>
2024-03-08 10:08:34 +0800

    add kube-system namespace

--- modules/karpenter/karpenter.tf
+++ modules/karpenter/karpenter.tf
@@ -4,8 +4,11 @@ module "karpenter" {
 
   cluster_name = var.cluster_name
 
-  irsa_oidc_provider_arn          = var.oidc_provider_arn
-  irsa_namespace_service_accounts = ["karpenter:karpenter"]
+  irsa_oidc_provider_arn = var.oidc_provider_arn
+  irsa_namespace_service_accounts = [
+    "karpenter:karpenter",
+    "kube-system:karpenter",
+  ]
 
   # create_iam_role      = false
   create_node_iam_role = false
