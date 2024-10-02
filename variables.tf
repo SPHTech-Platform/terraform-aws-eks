892d3ba95ea45c72b8b593a335347dffe527b524
Uchinda Padmaperuma <89894943+uchinda-sph@users.noreply.github.com>
2024-10-02 11:33:07 +0800

    fix: chart version and crd version mismatch

--- variables.tf
+++ variables.tf
@@ -525,7 +525,7 @@ variable "create_fargate_logging_policy_for_karpenter" {
 variable "karpenter_chart_version" {
   description = "Chart version for Karpenter"
   type        = string
-  default     = "v0.37.1"
+  default     = "0.37.4"
 }
 
 variable "karpenter_default_subnet_selector_tags" {
