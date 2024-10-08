020410d4e8c4c0602d2be68e5485a3ce568e4f4d
Uchinda Padmaperuma <89894943+uchinda-sph@users.noreply.github.com>
2024-10-09 00:53:02 +0800

    fix: nodepool consolidateAfter condition check

--- modules/karpenter/variables.tf
+++ modules/karpenter/variables.tf
@@ -90,8 +90,8 @@ variable "karpenter_nodepools" {
     ]
     karpenter_nodepool_disruption = {
       consolidation_policy = "WhenEmptyOrUnderutilized" # WhenEmpty or WhenEmptyOrUnderutilized
-      # consolidate_after    = "10m"                      # Only used if consolidation_policy is WhenEmpty
-      expire_after = "168h" # 7d | 168h | 1w
+      consolidate_after    = "5m"
+      expire_after         = "168h" # 7d | 168h | 1w
     }
     karpenter_nodepool_disruption_budgets = [{
       nodes = "10%"
