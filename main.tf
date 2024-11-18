b5633df7393d2a37e1dbf58e9b99305abf68214e
Uchinda Padmaperuma <89894943+uchinda-sph@users.noreply.github.com>
2024-11-18 16:07:36 +0800

    fix: addons logic

--- main.tf
+++ main.tf
@@ -1,57 +1,78 @@
 locals {
-  addon_vpc_cni_fargate_pod_identity = {
-    most_recent                 = true
-    resolve_conflicts_on_update = "OVERWRITE"
-    configuration_values = jsonencode({
-      env = {
-        # Reference doc: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html#security-groups-pods-deployment
-        ENABLE_POD_ENI                    = "true"
-        POD_SECURITY_GROUP_ENFORCING_MODE = "standard"
-      }
-      init = {
+  addon_vpc_cni = {
+    fargate_pod_identity = {
+      most_recent                 = true
+      resolve_conflicts_on_update = "OVERWRITE"
+      configuration_values = jsonencode({
         env = {
-          DISABLE_TCP_EARLY_DEMUX = "true"
+          # Reference doc: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html#security-groups-pods-deployment
+          ENABLE_POD_ENI                    = "true"
+          POD_SECURITY_GROUP_ENFORCING_MODE = "standard"
         }
-      }
-    })
-    pod_identity_association = [{
-      role_arn        = try(module.aws_vpc_cni_pod_identity[0].iam_role_arn, null)
-      service_account = "aws-node"
-    }]
-  }
-
-  addon_vpc_cni_fargate_irsa = {
-    most_recent                 = true
-    resolve_conflicts_on_update = "OVERWRITE"
-    configuration_values = jsonencode({
-      env = {
-        # Reference doc: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html#security-groups-pods-deployment
-        ENABLE_POD_ENI                    = "true"
-        POD_SECURITY_GROUP_ENFORCING_MODE = "standard"
-      }
-      init = {
+        init = {
+          env = {
+            DISABLE_TCP_EARLY_DEMUX = "true"
+          }
+        }
+      })
+      pod_identity_association = [{
+        role_arn        = try(module.aws_vpc_cni_pod_identity[0].iam_role_arn, null)
+        service_account = "aws-node"
+      }]
+    }
+    fargate_irsa = {
+      most_recent                 = true
+      resolve_conflicts_on_update = "OVERWRITE"
+      configuration_values = jsonencode({
         env = {
-          DISABLE_TCP_EARLY_DEMUX = "true"
+          # Reference doc: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html#security-groups-pods-deployment
+          ENABLE_POD_ENI                    = "true"
+          POD_SECURITY_GROUP_ENFORCING_MODE = "standard"
         }
-      }
-    })
-    service_account_role_arn = try(module.vpc_cni_irsa_role[0].iam_role_arn, null)
+        init = {
+          env = {
+            DISABLE_TCP_EARLY_DEMUX = "true"
+          }
+        }
+      })
+      service_account_role_arn = try(module.vpc_cni_irsa_role[0].iam_role_arn, null)
+    }
+    nodegroup_irsa = {
+      most_recent                 = true
+      resolve_conflicts_on_update = "OVERWRITE"
+      service_account_role_arn    = try(module.vpc_cni_irsa_role[0].iam_role_arn, null)
+    }
+    nodegroup_pod_identity = {
+      most_recent                 = true
+      resolve_conflicts_on_update = "OVERWRITE"
+      pod_identity_association = [{
+        role_arn        = try(module.aws_vpc_cni_pod_identity[0].iam_role_arn, null)
+        service_account = "aws-node"
+      }]
+    }
   }
 
-  addon_vpc_cni_ng_irsa = {
-    most_recent                 = true
-    resolve_conflicts_on_update = "OVERWRITE"
-    service_account_role_arn    = try(module.vpc_cni_irsa_role[0].iam_role_arn, null)
-  }
+  addon_vpc_cni_lookup = var.fargate_cluster && var.enable_pod_identity_for_eks_addons ? "fargate_pod_identity" : (
+    var.fargate_cluster ? "fargate_irsa" : (
+      var.enable_pod_identity_for_eks_addons ? "nodegroup_pod_identity" : "nodegroup_irsa"
+  ))
 
-  addon_vpc_cni_ng_pod_identity = {
-    most_recent                 = true
-    resolve_conflicts_on_update = "OVERWRITE"
-    pod_identity_association = [{
-      role_arn        = try(module.aws_vpc_cni_pod_identity[0].iam_role_arn, null)
-      service_account = "aws-node"
-    }]
+  addon_aws_ebs_csi_driver = {
+    pod_identity = {
+      most_recent                 = true
+      resolve_conflicts_on_update = "OVERWRITE"
+      pod_identity_association = [{
+        role_arn        = try(module.aws_ebs_csi_pod_identity[0].iam_role_arn, null)
+        service_account = "ebs-csi-controller-sa"
+      }]
+    }
+    irsa = {
+      most_recent                 = true
+      resolve_conflicts_on_update = "OVERWRITE"
+      service_account_role_arn    = try(module.ebs_csi_irsa_role[0].iam_role_arn, null)
+    }
   }
+  addon_aws_ebs_csi_drive_lookup = var.enable_pod_identity_for_eks_addons ? "pod_identity" : "irsa"
 }
 #tfsec:ignore:aws-eks-no-public-cluster-access-to-cidr
 #tfsec:ignore:aws-eks-no-public-cluster-access
@@ -121,22 +142,8 @@ module "eks" {
       most_recent                 = true
       resolve_conflicts_on_update = "OVERWRITE"
     }
-    vpc-cni = var.fargate_cluster && var.enable_pod_identity_for_eks_addons ? local.addon_vpc_cni_fargate_pod_identity : (
-      var.fargate_cluster ? local.addon_vpc_cni_fargate_irsa : (
-        var.enable_pod_identity_for_eks_addons ? local.addon_vpc_cni_ng_pod_identity : local.addon_vpc_cni_ng_irsa
-    ))
-    aws-ebs-csi-driver = var.enable_pod_identity_for_eks_addons ? {
-      most_recent                 = true
-      resolve_conflicts_on_update = "OVERWRITE"
-      pod_identity_association = [{
-        role_arn        = module.aws_ebs_csi_pod_identity[0].iam_role_arn
-        service_account = "ebs-csi-controller-sa"
-      }]
-      } : {
-      most_recent                 = true
-      resolve_conflicts_on_update = "OVERWRITE"
-      service_account_role_arn    = module.ebs_csi_irsa_role[0].iam_role_arn
-    }
+    vpc-cni            = lookup(local.addon_vpc_cni, local.addon_vpc_cni_lookup, {})
+    aws-ebs-csi-driver = lookup(local.addon_aws_ebs_csi_driver, local.addon_aws_ebs_csi_drive_lookup, {})
     coredns = var.fargate_cluster ? {
       most_recent                 = true
       resolve_conflicts_on_update = "OVERWRITE"
