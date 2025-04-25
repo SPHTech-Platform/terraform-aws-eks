160f7156b96dc57ffea7288f6714e94819bdb1ba
Uchinda Padmaperuma <89894943+uchinda-sph@users.noreply.github.com>
2025-04-25 12:09:00 +0800

    fix: localnode dns helm chart update to support IPv6 (#174)
    * fix: change the nodelocal dns helm chart to support IPv6
    
    * Update: README.md

--- modules/essentials/nodelocaldns.tf
+++ modules/essentials/nodelocaldns.tf
@@ -1,9 +1,20 @@
 locals {
   nodelocaldns_values = {
-    image_repository = var.nodelocaldns_image_repository
-    tag              = var.nodelocaldns_image_tag
-    local_dns_ip     = var.nodelocaldns_localdns_ip
-    affinity         = jsonencode(var.nodelocaldns_affinity)
+    image_repository         = var.nodelocaldns_image_repository
+    tag                      = var.nodelocaldns_image_tag
+    internel_domain_name     = var.nodelocaldns_internal_domain_name
+    kube_dns_svc_ip          = var.nodelocaldns_kube_dns_svc_ip
+    local_dns_ip             = var.nodelocaldns_localdns_ip
+    custom_upstream_svc_name = var.nodelocaldns_custom_upstream_svc_name
+    enable_logging           = var.nodelocaldns_enable_logging
+    no_ipv6_lookups          = var.ip_dual_stack_enabled ? var.nodelocaldns_no_ipv6_lookups : false
+    prefetch_enabled         = var.nodelocaldns_cache_prefetch_enabled
+    setup_interface          = var.nodelocaldns_setup_interface
+    setup_iptables           = var.nodelocaldns_setup_iptables
+    skip_teardown            = var.nodelocaldns_skip_teardown
+    pod_resources            = jsonencode(var.nodelocaldns_pod_resources)
+    affinity                 = jsonencode(var.nodelocaldns_affinity)
+    image_pull_secrets       = jsonencode(var.nodelocaldns_image_pull_secrets)
   }
 }
 
