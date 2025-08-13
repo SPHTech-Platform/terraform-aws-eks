599aabcc8900c49e12adb1a0c59ef87f0239fc7c
Uchinda Padmaperuma <89894943+uchinda-sph@users.noreply.github.com>
2025-08-13 14:09:07 +0800

    chore: update helm module version

--- modules/essentials/node_exporter.tf
+++ modules/essentials/node_exporter.tf
@@ -33,7 +33,7 @@ module "helm_node_exporter" {
   count = var.node_exporter_enabled ? 1 : 0
 
   source  = "SPHTech-Platform/release/helm"
-  version = "~> 0.1.4"
+  version = "~> 0.2.0"
 
   helm_config = local.node_exporter_helm_config
 
