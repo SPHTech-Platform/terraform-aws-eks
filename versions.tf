79e7bbd4850f7ea94e814d96f6f6e4ee2380275c
Uchinda Padmaperuma <89894943+uchinda-sph@users.noreply.github.com>
2025-10-22 11:30:35 +0800

    Update the aws prover version to > 6.0

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.70"
+      version = ">= 6.0"
     }
     # tflint-ignore: terraform_unused_required_providers
     kubernetes = {
