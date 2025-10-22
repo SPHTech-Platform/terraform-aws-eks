7c9b95dd7d2ba95676d75bee97d6da0b7b8ed2c2
Uchinda Padmaperuma <89894943+uchinda-sph@users.noreply.github.com>
2025-10-22 11:57:06 +0800

    chore: update cluster_version validation upto 1.33 cluster version (#181)
    * chore: update cluster_version validation upto 1.33 cluster version
    
    * Update tflint
    
    * chore: update aws provider version
    
    * fix: typo

--- modules/self_managed_nodes/versions.tf
+++ modules/self_managed_nodes/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.70"
+      version = ">= 5.70, < 6.0"
     }
     time = {
       source  = "hashicorp/time"
