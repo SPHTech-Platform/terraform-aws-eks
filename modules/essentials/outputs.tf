aaabecd2cecb1f0043350a3e8e444aa7fecf98ba
Uchinda Padmaperuma <89894943+uchinda-sph@users.noreply.github.com>
2026-01-16 12:54:02 +0800

    [PFMENG-4251] chore: eks essential output

--- modules/essentials/outputs.tf
+++ modules/essentials/outputs.tf
@@ -2,3 +2,8 @@ output "node_termination_handler_sqs_arn" {
   description = "ARN of the SQS queue used to handle node termination events"
   value       = try(module.node_termination_handler_sqs[0].queue_arn, "")
 }
+
+output "fluent_bit_irsa_id" {
+  description = "ID of the IAM Role for Service Account used by Fluent Bit"
+  value       = try(aws_iam_policy.fluent_bit_irsa[0].policy_id, "")
+}
