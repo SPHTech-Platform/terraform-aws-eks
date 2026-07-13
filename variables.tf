dfffc57bae3e11009f527302efc960d20f8cbc0f
Uchinda Padmaperuma <uchinda@sph.com.sg>
2026-07-13 12:16:31 +0800

    fix: restrict Zonal Shift permissions by adding a resource-level condition in Karpenter IRSA policy

--- variables.tf
+++ variables.tf
@@ -555,13 +555,13 @@ variable "create_fargate_logging_policy_for_karpenter" {
 variable "karpenter_chart_version" {
   description = "Chart version for Karpenter"
   type        = string
-  default     = "1.12.0"
+  default     = "1.14.0"
 }
 
 variable "karpenter_crd_chart_version" {
   description = "Chart version for Karpenter CRDs same version as `karpenter_chart_version`"
   type        = string
-  default     = "1.12.0"
+  default     = "1.14.0"
 }
 
 variable "karpenter_default_subnet_selector_tags" {
